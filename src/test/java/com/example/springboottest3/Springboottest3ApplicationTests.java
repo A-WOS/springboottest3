462efa523b256e7b1dea3baae9f62df10b106bac
Wonseok0331 <uws0405@gmail.com>
2020-10-03 09:55:44 +0900

    init commit

---
+++ src/test/java/com/example/springboottest3/Springboottest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboottest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
