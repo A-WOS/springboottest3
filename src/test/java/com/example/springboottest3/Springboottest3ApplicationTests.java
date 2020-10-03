a55ae174603340aae80df228035e65388f16c67e
Wonseok0331 <uws0405@gmail.com>
2020-10-03 10:20:32 +0900

    default

--- src/test/java/com/example/springboottest3/Springboottest3ApplicationTests.java
+++ src/test/java/com/example/springboottest3/Springboottest3ApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.springboottest3;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class Springboottest3ApplicationTests {
 
