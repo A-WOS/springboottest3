648228e135050df1db811d26727e02e6ea030d59
Wonseok0331 <uws0405@gmail.com>
2020-10-05 14:10:47 +0900

    front와 backend처리

--- src/main/java/com/example/springboottest3/controller/guguController.java
+++ src/main/java/com/example/springboottest3/controller/guguController.java
@@ -9,13 +9,19 @@ import org.springframework.web.bind.annotation.*;
 
 // 컨트롤러 구현
 @Controller
-@RequestMapping(value="/front", method=RequestMethod.GET)//, method = RequestMethod.GET) // /front라는 경로로 들어오는 것을 정의
+@RequestMapping(method=RequestMethod.GET)//, method = RequestMethod.GET) // /front라는 경로로 들어오는 것을 정의
 public class guguController {
 
-    @GetMapping("/gugu") // gugu라는 함수의 mapping 경로는 /front/gugu가 됨.
-    public String gugu(@RequestParam(name="key", required = false, defaultValue = "World") String key, Model model) {
+    @GetMapping("/front/gugu") // gugu라는 함수의 mapping 경로는 /front/gugu가 됨.
+    public String guguf(@RequestParam(name="key", required = false, defaultValue = "World") String key, Model model) {
         model.addAttribute("key", key);
-        return "gugu"; // html name
+        return "guguf"; // html name
+
+    }
+    @GetMapping("/backend/gugu") // gugu라는 함수의 mapping 경로는 /backend/gugu가 됨.
+    public String gugub(@RequestParam(name="key", required = false, defaultValue = "World") String key, Model model) {
+        model.addAttribute("key", key);
+        return "gugub"; // html name
 
     }
 
