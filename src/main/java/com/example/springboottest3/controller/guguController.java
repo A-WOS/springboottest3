c1f8bdb80d2735cf70730f3c9ed781e9ce9c96a0
Wonseok0331 <uws0405@gmail.com>
2020-10-05 13:51:58 +0900

    gugu 컨트롤러 구현

--- src/main/java/com/example/springboottest3/controller/guguController.java
+++ src/main/java/com/example/springboottest3/controller/guguController.java
@@ -1,13 +1,15 @@
 package com.example.springboottest3.controller;
 
-import com.example.springboottest3.gugudan;
+import lombok.NonNull;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 // 컨트롤러 구현
 @Controller
-@RequestMapping(value="/front")//, method = RequestMethod.GET) // /front라는 경로로 들어오는 것을 정의
+@RequestMapping(value="/front", method=RequestMethod.GET)//, method = RequestMethod.GET) // /front라는 경로로 들어오는 것을 정의
 public class guguController {
 
     @GetMapping("/gugu") // gugu라는 함수의 mapping 경로는 /front/gugu가 됨.
@@ -16,4 +18,9 @@ public class guguController {
         return "gugu"; // html name
 
     }
+
+
 }
+
+
+
