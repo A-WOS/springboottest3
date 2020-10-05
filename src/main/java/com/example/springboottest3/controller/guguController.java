29c642d760dd5b3a7c6ad25eacae3988159d9e4a
Wonseok0331 <uws0405@gmail.com>
2020-10-05 12:25:22 +0900

    /front/gugu?key=5 까지 진행

--- src/main/java/com/example/springboottest3/controller/guguController.java
+++ src/main/java/com/example/springboottest3/controller/guguController.java
@@ -2,19 +2,18 @@ package com.example.springboottest3.controller;
 
 import com.example.springboottest3.gugudan;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 // 컨트롤러 구현
 @Controller
-@RequestMapping(method=RequestMethod.GET, value="/front/gugu?key={num}")
+@RequestMapping(value="/front")//, method = RequestMethod.GET) // /front라는 경로로 들어오는 것을 정의
 public class guguController {
-    public String gugu(@PathVariable("num") int num) {
-        gugudan gudan = new gugudan(num);
-        gudan.cal();
 
-        return "gugu";
+    @GetMapping("/gugu") // gugu라는 함수의 mapping 경로는 /front/gugu가 됨.
+    public String gugu(@RequestParam(name="key", required = false, defaultValue = "World") String key, Model model) {
+        model.addAttribute("key", key);
+        return "gugu"; // html name
 
     }
 }
