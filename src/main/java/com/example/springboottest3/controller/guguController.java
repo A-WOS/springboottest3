cd19a076fe72af6a81686a2f67d02011d3b11ca1
Wonseok0331 <uws0405@gmail.com>
2020-10-06 06:26:01 +0900

    backend/gugu/{key} 구구단 출력 기능 추가

--- src/main/java/com/example/springboottest3/controller/guguController.java
+++ src/main/java/com/example/springboottest3/controller/guguController.java
@@ -1,25 +1,36 @@
 package com.example.springboottest3.controller;
 
+import com.example.springboottest3.component.GugudanComp;
+import com.example.springboottest3.dto.GugudanDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 // 컨트롤러 구현
 @Controller
 @RequestMapping(method=RequestMethod.GET)//, method = RequestMethod.GET) // /front라는 경로로 들어오는 것을 정의
 public class guguController {
+    @Autowired private GugudanComp gugudanComp;
+
     @GetMapping("/front/gugu") // gugu라는 함수의 mapping 경로는 /front/gugu가 됨.
-    public String guguf(Model model, @RequestParam(name = "key", required = false, defaultValue = "5") int key) {
+    public String dispGuguf(Model model, @RequestParam(name = "key", required = false, defaultValue = "5") int key) {
         model.addAttribute("key", key);
 
-        return "guguf"; // html name
+        return "front/guguf"; // html name
     }
 
     @GetMapping("/backend/gugu") // gugu라는 함수의 mapping 경로는 /backend/gugu가 됨.
-    public String gugub(Model model, @RequestParam(name = "key", required = false, defaultValue = "5") int key) {
-        model.addAttribute("key", key);
+    public String dispBackendGugu(Model model, @RequestParam(name = "key", required = false, defaultValue = "5") int key) {
+        // Init
+        List<GugudanDto> listGugudan = gugudanComp.createDan(key);
 
-        return "gugub"; // html name
+        model.addAttribute("key", key);
+        model.addAttribute("listGugudan", listGugudan);
+
+        return "back/gugub"; // html name
     }
 
     @GetMapping("/front/gugu/{key}")
@@ -28,6 +39,16 @@ public class guguController {
 
         return "front/guguFK";
     }
+
+    @GetMapping("/backend/gugu/{key}")
+    public String dispBackGuguList(Model model, @PathVariable("key") int key) {
+        List<GugudanDto> listGuguBack = gugudanComp.createDan2(key);
+        model.addAttribute("key", key);
+        model.addAttribute("listGuguBack", listGuguBack);
+
+        return "back/guguBK";
+    }
+
 }
 
 
---
+++ src/main/java/com/example/springboottest3/dto/GugudanDto.java
@@ -0,0 +1,15 @@
+package com.example.springboottest3.dto;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+public class GugudanDto {
+
+    private int dan1;
+    private int dan2;
+    private int result;
+
+}
