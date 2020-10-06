d52cc740fa2ee439038140f5ce4bb1039f31bd86
Wonseok0331 <uws0405@gmail.com>
2020-10-06 17:51:21 +0900

    backend 1-2 컨트롤러 추가

--- src/main/java/com/example/springboottest3/controller/guguController.java
+++ src/main/java/com/example/springboottest3/controller/guguController.java
@@ -1,12 +1,18 @@
 package com.example.springboottest3.controller;
 
+import com.example.springboottest3.component.guguComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import com.example.springboottest3.Dto.guguDto;
+import java.util.List;
 
 @Controller
 public class guguController {
-   // @Auto
+    // Dependency Injection(DI) : 의존성 주입.
+    // @Autowired를 이용하여 guguComponet객체를 사용.
+    @Autowired guguComponent gcp;
     // 1-1
     @GetMapping("/front/gugu")
     public String guguFrontIsKey(Model model, @RequestParam("key") int key){
@@ -15,6 +21,13 @@ public class guguController {
     }
 
     // 1-2
+    @GetMapping("/back/gugu")
+    public String guguBackIsKey(Model model, @RequestParam("key") int key){
+        List<guguDto> listgugudan = gcp.createDan(key);
+        model.addAttribute("listgugudan", listgugudan);
+        model.addAttribute("key", key);
+        return "backEnd/guguBackKey";
+    }
 
 
     // 2-1
@@ -25,6 +38,7 @@ public class guguController {
     }
 
     // 2-2
+
 
 
 
