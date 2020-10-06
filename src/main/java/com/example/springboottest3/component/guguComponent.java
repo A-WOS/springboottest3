7670508b5b1b8792c3d283c1ca499fd276334cd7
Wonseok0331 <uws0405@gmail.com>
2020-10-06 15:53:04 +0900

    gugudan component setting

---
+++ src/main/java/com/example/springboottest3/component/guguComponent.java
@@ -0,0 +1,15 @@
+package com.example.springboottest3.component;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Getter
+@Setter
+public class guguComponent {
+    private int dan1;
+    private int dan2;
+    private int result;
+
+}
