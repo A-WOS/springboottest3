58653ed8a56b02e68cf92dd42e4461869dfd638a
Wonseok0331 <uws0405@gmail.com>
2020-10-06 16:09:34 +0900

    getter setter constructor

---
+++ src/main/java/com/example/springboottest3/Dto/guguDto.java
@@ -0,0 +1,14 @@
+package com.example.springboottest3.Dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class guguDto {
+    private int dan1;
+    private int dan2;
+    private int result;
+
+}
